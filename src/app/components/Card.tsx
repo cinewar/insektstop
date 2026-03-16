b96bc965a6fbcd46e38391de63e8ed7eb04abfc3
cinewar <49623430+cinewar@users.noreply.github.com>
2026-03-17 00:22:48 +0300

    Fix for Card clik

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,11 +1,13 @@
 interface CardProps {
   children: React.ReactNode;
   className?: string;
+  onClick?: () => void;
 }
 
-export function Card({children, className}: CardProps) {
+export function Card({children, className, onClick}: CardProps) {
   return (
     <div
+      onClick={onClick}
       className={`bg-tertiary min-w-45 p-1 text-white shadow-lg rounded-xl ${className}`}
     >
       {children}
--- src/app/components/Cards.tsx
+++ src/app/components/Cards.tsx
@@ -1,38 +1,44 @@
 import Image from 'next/image';
 import products from '@/lib/products.json';
-import Link from 'next/link';
 import {Card} from './Card';
 import Svg from './Svg';
 import {RIGHTARROWSVG} from '../utils/svg';
+import Link from 'next/link';
 
 export function Cards() {
   return (
     <div className='flex gap-3 w-full py-4 px-2 overflow-auto no-scrollbar'>
       {products.map((product) => (
-        <Card key={product.id} className='relative'>
-          <h2 className='absolute text-white font-bold shadow-lg top-2 right-2 bg-tertiary p-1 rounded-full'>
-            ${product.price.toFixed(2)}
-          </h2>
-          <Image
-            src={product.image}
-            alt={product.name}
-            width={200}
-            height={200}
-            className='rounded-lg'
-          />
-          <div className='p-2 h-30 justify-between flex flex-col'>
-            <div className='font-bold mb-1'>
-              <h2>{product.name}</h2>
-              {/* <h2>${product.price.toFixed(2)}</h2> */}
+        <Link
+          key={product.id}
+          href={`/products/${product.id}`}
+          className='relative'
+        >
+          <Card key={product.id} className='relative'>
+            <h2 className='absolute text-white font-bold shadow-lg top-2 right-2 bg-tertiary p-1 rounded-full'>
+              ${product.price.toFixed(2)}
+            </h2>
+            <Image
+              src={product.image}
+              alt={product.name}
+              width={200}
+              height={200}
+              className='rounded-lg'
+            />
+            <div className='p-2 h-30 justify-between flex flex-col'>
+              <div className='font-bold mb-1'>
+                <h2>{product.name}</h2>
+                {/* <h2>${product.price.toFixed(2)}</h2> */}
+              </div>
+              <div className='flex items-end'>
+                <p className='line-clamp-2'>{product.description}</p>
+                <p>
+                  <Svg icon={RIGHTARROWSVG} size={24} />
+                </p>
+              </div>
             </div>
-            <div className='flex items-end'>
-              <p className='line-clamp-2'>{product.description}</p>
-              <Link href={`/products/${product.id}`}>
-                <Svg icon={RIGHTARROWSVG} size={24} />
-              </Link>
-            </div>
-          </div>
-        </Card>
+          </Card>
+        </Link>
       ))}
     </div>
   );
