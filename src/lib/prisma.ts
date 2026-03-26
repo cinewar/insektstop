b26583b8e85fcc82bf0c5ec102e2a7086c1f28d7
cinewar <49623430+cinewar@users.noreply.github.com>
2026-03-26 23:03:50 +0300

    fix for deployment to vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,11 @@
-import 'dotenv/config';
 import {PrismaClient} from '../../generated/prisma/client';
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {prisma: PrismaClient};
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 export {prisma};
