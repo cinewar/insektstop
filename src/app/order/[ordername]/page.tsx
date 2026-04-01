73c1c6dac44cf4de82f2a4302fd2bf1a6e9b5f29
cinewar <49623430+cinewar@users.noreply.github.com>
2026-04-01 19:08:55 +0300

    Order page adjusted

---
+++ src/app/order/[ordername]/page.tsx
@@ -0,0 +1,40 @@
+import {prisma} from '@/lib/prisma';
+import OrderItemsAccordion from '../../components/OrderItemsAccordion';
+
+export default async function Order() {
+  const products = await prisma.product.findMany();
+  const order = await prisma.order.findFirst({
+    where: {
+      orderName: 'Sample Order',
+    },
+    include: {
+      orderItems: {
+        include: {
+          products: {
+            include: {
+              product: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  console.log('Order:', order);
+
+  const orderItems = order?.orderItems ?? [];
+
+  const totalPrice = orderItems.reduce((sum, item) => sum + item.price, 0);
+
+  return (
+    <div className='min-h-screen flex bg-secondary'>
+      <div className='fixed h-28 px-3 pb-2 flex shadow-md items-end w-full max-w-120 bg-secondary z-20'>
+        <div className='flex items-center justify-between w-full gap-2 relative text-lg font-bold text-dark-text'>
+          <div className=''>Order: {order?.orderName}</div>
+          <div>Total Price: ${totalPrice.toFixed(2)}</div>
+        </div>
+      </div>
+      <OrderItemsAccordion items={orderItems} products={products} />
+    </div>
+  );
+}
--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,8 +1,6 @@
 import {prisma} from '@/lib/prisma';
-import OrderItemsAccordion from '../components/OrderItemsAccordion';
 
-export default async function Order() {
-  const products = await prisma.product.findMany();
+export default async function CreateOrder() {
   const order = await prisma.order.findFirst({
     where: {
       orderName: 'Sample Order',
@@ -21,20 +19,9 @@ export default async function Order() {
   });
 
   console.log('Order:', order);
-
-  const orderItems = order?.orderItems ?? [];
-
-  const totalPrice = orderItems.reduce((sum, item) => sum + item.price, 0);
-
   return (
-    <div className='min-h-screen flex bg-secondary'>
-      <div className='fixed h-28 px-3 pb-2 flex shadow-md items-end w-full max-w-120 bg-secondary z-20'>
-        <div className='flex items-center justify-between w-full gap-2 relative text-lg font-bold text-dark-text'>
-          <div className=''>Order: {order?.orderName}</div>
-          <div>Total Price: ${totalPrice.toFixed(2)}</div>
-        </div>
-      </div>
-      <OrderItemsAccordion items={orderItems} products={products} />
+    <div className='min-h-screen flex items-center justify-center bg-secondary'>
+      <h1 className='text-4xl font-bold text-dark-text'>Create Order</h1>
     </div>
   );
 }
