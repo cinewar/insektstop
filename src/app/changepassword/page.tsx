f6a92f9dd5892ffe3b79c407c3b12d69c1f1d36a
cinewar <49623430+cinewar@users.noreply.github.com>
2026-04-21 06:17:06 +0300

    fix for padding

--- src/app/changepassword/page.tsx
+++ src/app/changepassword/page.tsx
@@ -2,7 +2,7 @@ import {ChangePassword} from '@/app/changepassword/components/ChangePassword';
 
 export default function ChangePasswordPage() {
   return (
-    <div className='flex flex-col mt-24 h-screen gap-4 px-4 bg-secondary'>
+    <div className='flex flex-col pt-24 h-screen gap-4 px-4 bg-secondary'>
       <h1 className='text-2xl font-bold'>Şifre Değişikliği</h1>
 
       <ChangePassword />
