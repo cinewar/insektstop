88e8d14a4085241064309792c32a70278f41bfea
cinewar <49623430+cinewar@users.noreply.github.com>
2026-04-22 22:50:45 +0300

    fix for admin logout redirect

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,22 @@
+import {prisma} from '@/lib/prisma';
+import {getSessionUser} from '../lib/session';
 import {AdminHeader} from './components/AdminHeader';
+import {redirect} from 'next/navigation';
 
-export default function AdminLayout({children}: {children: React.ReactNode}) {
+export default async function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const adminUser = (await getSessionUser()) as {email: string} | null;
+  const user = await prisma.user.findFirst({
+    where: {
+      email: adminUser?.email,
+    },
+  });
+  if (!user || !adminUser || adminUser.email !== user.email) {
+    redirect('/'); // Admin değilse ana sayfaya yönlendir
+  }
   return (
     <div className='min-h-screen bg-secondary'>
       <AdminHeader />
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,20 +1,17 @@
 import {prisma} from '@/lib/prisma';
 import {getSessionUser} from '../lib/session';
-import {redirect} from 'next/navigation';
 import {User} from './components/User';
 
 export default async function AdminPage() {
   const adminUser = (await getSessionUser()) as {email: string} | null;
-  console.log('Admin user:', adminUser);
   const user = await prisma.user.findFirst({
     where: {
       email: adminUser?.email,
     },
   });
-  if (!user || !adminUser || adminUser.email !== user.email) {
-    redirect('/'); // Admin değilse ana sayfaya yönlendir
+  if (!user) {
+    return <div>User not found.</div>;
   }
-
   return (
     <>
       <User user={user} />
