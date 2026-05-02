aca86642d9fbc0100b3923e5490ec48038486638
cinewar <49623430+cinewar@users.noreply.github.com>
2026-05-02 12:28:37 +0300

    build fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,9 @@
-import {prisma} from '@/lib/prisma';
 import {ContactContent} from './ContactContent';
 
 export default async function Contact() {
-  const user = await prisma.user.findFirst({});
-
-  return <>{user && <ContactContent user={user} />}</>;
+  return (
+    <>
+      <ContactContent />
+    </>
+  );
 }
